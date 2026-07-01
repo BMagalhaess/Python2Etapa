cb03a02aed19e054ec674d8caff96100edf436c1
BERNARDO MAGALHAES <bernardo.maga2009@gmail.com>
2026-07-01 09:24:01 -0300

    Delete B_cinema directory

--- B_cinema/models/base.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-
-class ModeloBase(db.Model):
-    """Classe base abstrata: todo modelo ganha um id automático."""
-
-    __abstract__ = True
-
-    id = db.Column(db.Integer, primary_key=True)
